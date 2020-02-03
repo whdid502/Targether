15dbe4972f723780a7c36f700957f9145e264243
whdid502 <whdid1234@naver.com>
2020-02-03 16:26:50 +0900

    nav의 js안되는것 수정,스크립트 순서오류엿음, 파이선 templates&static 복사본 저장

--- Game_Sale_info_pythoncode/app.py
+++ Game_Sale_info_pythoncode/app.py
@@ -1,10 +1,28 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/game_sale')
+def game_main_page():
+   return render_template('game_sales.html')
+
+@app.route('/page_steam')
+def game_steam_page():
+   return render_template('steam_page.html')
+
 @app.route('/steam_info', methods=['GET'])
 def get_steam_sale():
   client = MongoClient('localhost', 27017)
   db = client.dbstemainfo
   output = []
   for s in db.sale.find():
-    output.append({'steam_img': s['steam_img'], 'steam_title' : s['steam_title'], 'steam_orginal_price' : s['steam_original_price'], 'steam_discount_rate' : s['steam_discount_rate'] , 'steam_discount_price' : s['steam_discount_price']})
+    output.append({'steam_link' : s['steam_link'], 'steam_img': s['steam_img'], 'steam_title' : s['steam_title'], 'steam_original_price' : s['steam_original_price'], 'steam_discount_rate' : s['steam_discount_rate'] , 'steam_discount_price' : s['steam_discount_price']})
   return jsonify({'result' : output})
 
 @app.route('/steam_info', methods=['POST'])
@@ -14,6 +32,7 @@ def steam_sale():
     soup = BeautifulSoup(r.text, 'html.parser')
     games = soup.select('div#search_resultsRows > a')
     for game in games:
+        game_link = game['href']
         game_img = game.select_one('div.search_capsule > img').get('src')
         game_title = game.select_one('div.responsive_search_name_combined >  div.col.search_name.ellipsis > span').text
         game_original_pirce = game.select_one('div.responsive_search_name_combined > div.col.search_price_discount_combined.responsive_secondrow > div.col.search_price.discounted.responsive_secondrow > span > strike').text
@@ -25,14 +44,23 @@ def steam_sale():
         db = client.dbsteaminfo
         db.info.delete_many({}) # db를 초기화 하기 위한 삭제입니다.
         doc = {
+            'steam_link' : game_link,
             'steam_img' : game_img,
             'steam_title' : game_title,
-            'steam_orginal_price': game_original_pirce,
+            'steam_original_price': game_original_pirce,
             'steam_discount_rate' : game_discount_rate,
             'steam_discount_price' : combined_price.text
         }
         db.info.insert_one(doc)
-        for s in db.info.find(): # get을 섞었습니다
-            get_steam_info = []
-            get_steam_info.append({'steam_img': s['steam_img'], 'steam_title': s['steam_title'], 'steam_orginal_price': s['steam_orginal_price'], 'steam_discount_rate': s['steam_discount_rate'], 'steam_discount_price': s['steam_discount_price'})
-        return jsonify({'result':'success', 'msg': '성공적으로 저장됐습니다.', 'output': get_steam_info})
+        return jsonify({'result': 'success'})
+
+        # for s in db.info.find(): # get을 섞었습니다
+        #     get_steam_info = []
+        #     get_steam_info.append({'steam_link' : s['steam_link'], 'steam_img': s['steam_img'], 'steam_title': s['steam_title'], 'steam_original_price': s['steam_original_price'], 'steam_discount_rate': s['steam_discount_rate'], 'steam_discount_price': s['steam_discount_price']})
+        # return jsonify({'result':'success', 'msg': '성공적으로 저장됐습니다.', 'output': get_steam_info})
+
+
+
+
+if __name__ == '__main__':
+   app.run('0,0,0,0',port=5000,debug=True)
